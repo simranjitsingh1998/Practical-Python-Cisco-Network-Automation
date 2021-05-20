bfc6f6cc068d200c442644cfaf71106eca85a04c
simranjitsingh1998 <83768203+simranjitsingh1998@users.noreply.github.com>
2021-05-20 11:33:09 -0400

    Telnet Library with Cisco Routers

---
+++ Using Loops to Create Vlans Python.py
@@ -0,0 +1,25 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.122.100"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+for n in range(0,11):
+	tn.write(b"vlan " + str(n).encode("ascii") + "\n".encode("ascii"))
+	tn.write(b"name Python_Vlan_" + str(n).encode("ascii") + "\n".encode("ascii"))	
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
